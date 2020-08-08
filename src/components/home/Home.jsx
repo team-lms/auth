ae389834448e5254dd6db6d53a95d1498b0ba875
Rohit Sharma <rohit2sharma95@gmail.com>
2020-08-08 16:52:32 +0530

    Change buttons into Links

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
   <div className="vh-100 bg-main">
@@ -17,8 +18,8 @@ const Home = () => (
             way possible
           </h1>
           <p className="lead">Get ready to experience the beautiful UI</p>
-          <button type="button" className="btn btn-lg btn-primary px-5 mr-3">Sign in</button>
-          <button type="button" className="btn btn-lg btn-outline-primary px-5">Help</button>
+          <Link to="/login" className="btn btn-lg btn-primary px-5 mr-3">Sign in</Link>
+          <Link to="/" className="btn btn-lg btn-outline-primary px-5">Help</Link>
         </div>
       </div>
     </div>
