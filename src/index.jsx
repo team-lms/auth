fd10a0e4f4cebba9f93eb29b3d55c1187e787d18
Rohit Sharma <rohit2sharma95@gmail.com>
2020-08-09 11:56:55 +0530

    Implement store
    - Create store file
    - Provide the store to the application

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './helper/store';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={ store }>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
